31470e400f5c12145d543d020cc264cb045943bf
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2020-12-30 13:23:07 -0500

    added logo component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,5 +3,15 @@ import React from "react";
 import "../style/logo.css";
 
 export default function Logo() {
-  return <div className="logo">Logo</div>;
+  return (
+    <div className="logo">
+      <div className="logo__img-container">
+        <img
+          src="https://www.vmcdn.ca/f/files/via/import/2017/11/23164825_homer-simpson-doughnut.jpg;w=960"
+          alt="logo"
+        />
+      </div>
+      <div className="logo__title">e-Grocery</div>
+    </div>
+  );
 }
