79013b1719c61c4d9029a200a6952ea315820746
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2020-12-31 09:13:03 -0500

    touched up media querys, starting on the cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "../style/cart.css";
 
+import Header from "../components/Header.js";
+import Inventory from "../components/Inventory.js";
+import Footer from "../components/Footer.js";
+
 export default function Cart() {
-  return <div className="cart">3</div>;
+  return (
+    <div className="cart">
+      <Header />
+      <Inventory />
+      <Footer />
+    </div>
+  );
 }
