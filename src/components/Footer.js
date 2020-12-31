4af5d6893f8ba459b99c5f86f5eff9468edd422d
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2020-12-31 09:38:39 -0500

    added simple footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,5 +3,14 @@ import React from "react";
 import "../style/footer.css";
 
 export default function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <div className="footer__total">
+        Total <strong>$0.00</strong>
+      </div>
+      <div className="footer__btn">
+        <button>Checkout</button>
+      </div>
+    </div>
+  );
 }
