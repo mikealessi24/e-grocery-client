a99533ef5be82527a31008b1b3695fe053abd2e6
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2021-01-01 10:33:52 -0500

    added simple inventory item in cart

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import InventoryItem from "../components/InventoryItem";
+
 import "../style/inventory.css";
 
 export default function Inventory() {
-  return <div className="inventory"></div>;
+  return (
+    <div className="inventory">
+      <InventoryItem />
+    </div>
+  );
 }
---
+++ src/components/InventoryItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "../style/inventoryItem.css";
+
+export default function InventoryItem() {
+  return (
+    <div className="inventoryItem">
+      <div className="inventoryItem__description-container">
+        <div className="inventoryItem__img-container">
+          <img
+            src="https://i5.walmartimages.com/asr/e575de6b-a4fa-4887-b12b-68a3b4cf37e0_1.72e90350d9498b5a6bffd62755e5ebe2.jpeg?odnWidth=215&odnHeight=215&odnBg=ffffff"
+            alt="item"
+          />
+        </div>
+        <div className="inventoryItem__description">blah blah</div>
+      </div>
+      <div className="inventoryItem__info">
+        <div>quantity</div>
+        <div>price</div>
+      </div>
+    </div>
+  );
+}
