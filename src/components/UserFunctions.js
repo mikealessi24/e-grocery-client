110ab8a7daa2d61daa8e2c36ebd054838c455dd4
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2021-01-03 12:44:36 -0500

    added material/ui and sign up modal skeleton

--- src/components/UserFunctions.js
+++ src/components/UserFunctions.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import "../style/userfunctions.css";
 
+import SignInModal from "../components/SignInModal";
+
 export default function UserFunctions() {
   return (
     <div className="userfunctions">
-      <div>Sign In</div>
+      <SignInModal />
       <div>favorites</div>
       <div>$0.00</div>
     </div>
