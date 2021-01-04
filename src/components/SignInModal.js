42b722b8829df7ef65e0369652c674657e8d1b38
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2021-01-04 12:01:11 -0500

    working on client sign up/sign in

--- src/components/SignInModal.js
+++ src/components/SignInModal.js
@@ -2,25 +2,13 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
+import { Auth } from "aws-amplify";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: "absolute",
-    left: "50%",
+    left: "25%",
+    top: "25%",
     width: 800,
     height: 500,
     backgroundColor: theme.palette.background.paper,
@@ -31,8 +19,6 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SimpleModal() {
   const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
@@ -43,13 +29,58 @@ export default function SimpleModal() {
     setOpen(false);
   };
 
+  async function signUp(e) {
+    e.preventDefault();
+    console.log(e.target.elements);
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
+    const email = e.target.elements.email.value;
+    console.log(username);
+    try {
+      const { user } = await Auth.signUp({
+        username,
+        password,
+        attributes: {
+          email,
+        },
+      });
+      console.log(user);
+    } catch (error) {
+      console.log("error signing up:", error);
+    }
+  }
+
+  async function confirmSignUp(e) {
+    const username = e.target.elements.username.value;
+    const code = e.target.elements.username.value;
+    try {
+      await Auth.confirmSignUp(username, code);
+    } catch (error) {
+      console.log("error confirming sign up", error);
+    }
+  }
+
   const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Text in a modal</h2>
-      <p id="simple-modal-description">
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
-      <SimpleModal />
+    <div className={classes.paper}>
+      <h2 id="simple-modal-title">sign in</h2>
+      <div id="simple-modal-description">
+        <form>
+          <input type="text" />
+          <input type="text" />
+        </form>
+        <br />
+        <form onSubmit={(e) => signUp(e)}>
+          <input type="text" placeholder="sign-up" name="username" />
+          <input type="text" name="password" />
+          <input type="text" name="email" />
+          <button type="submit">submit</button>
+        </form>
+        <form onSubmit={(e) => confirmSignUp(e)}>
+          <input type="text" placeholder="username" name="username" />
+          <input type="text" placeholder="code" name="code" />
+          <button type="submit">submit</button>
+        </form>
+      </div>
     </div>
   );
 
